3350541da6e1e34153d80d03ac58e82a23ede429
David Raynes <rayners@gmail.com>
2016-11-28 20:25:11 -0500

    $log.info instead of console.log

--- public/fswd/submodule.js
+++ public/fswd/submodule.js
@@ -1,9 +1,8 @@
 var angular = require('angular');
 
 angular.module('fswd.submodule', [require('angular-route/index')])
-  .config(function($routeProvider) {
-    console.log($routeProvider);
-    console.log('I am in a submodule!');
+  .run(function($log) {
+    $log.info('I am in a submodule!');
   });
 
 module.exports = angular.module('fswd.submodule');
